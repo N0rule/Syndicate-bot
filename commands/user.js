5be799ccb2013a319986b7821754508c11fcd625
N0rule <N0rule228@gmail.com>
2022-11-14 02:30:39 +0200

    tired

--- commands/user.js
+++ commands/user.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
-		.setDescription('Дает информацию о пользователе.'),
+		.setDescription('Дает информацию о пользователе'),
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
