03ddb8f8ebe363fec73ddb0bd392831661c8926a
N0rule <N0rule228@gmail.com>
2022-11-14 02:09:43 +0200

    Added slash (/) commands and all its dependencies

---
+++ commands/user.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('Дает информацию о пользователе.'),
+	async execute(interaction) {
+		// interaction.user is the object representing the User who ran the command
+		// interaction.member is the GuildMember object, which represents the user in the specific guild
+		await interaction.reply(`Эта команда была использована ${interaction.user.tag}, который присоединился ${interaction.member.joinedAt}.`);
+	},
+};
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const {Client,GatewayIntentBits, } = require ("discord.js")
+const fs = require('node:fs');
+const path = require('node:path');
+const {Client,Events,Collection,GatewayIntentBits,} = require ("discord.js")
 const { default: mongoose } = require("mongoose")
 const mangoose = require("mongoose")
 require("dotenv/config")
@@ -13,15 +15,51 @@ intents: [
 ],
 });
 
+client.commands = new Collection();
+
+//command Hendler
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+//----------------------------------------------------------
+
+
 client.on('ready', () => {
     console.log('Бот Готов к роботе')
-
+//mongodb connect
     mongoose.connect(process.env.MONGO_URI ,{
 
     keepAlive: true
     });
 });
+//Executing commands
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
+//adding string to mongodb
 client.on('messageCreate', async (message) => {
         await messageCountSchema.findOneAndUpdate({
             _id: message.author.id
@@ -40,11 +78,6 @@ client.on('messageCreate', async (message) => {
         },
         )
 });
+//--------------------------
 
-client.on('messageCreate', (message) => {
-
-    if (message.content === 'ping') {
-        message.reply ('pong')
-    }
-});
 client.login(process.env.TOKEN);
